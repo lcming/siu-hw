ff9de4f7243fbe8def9e87dba18bbacd2d3a6d6f
cm@twins <cm99585@hotmail.com>
2015-12-09 17:39:17 +0800

    parameter scalar

--- ccc/Scalar.v
+++ ccc/Scalar.v
@@ -5,6 +5,12 @@
 `include "DW01_mux_any.v"
 //synopsys translate_on
 
+`define ADDR_W 4
+`define MEM_W (1 << ADDR_W)
+
+`define BASE 9  //  bit base for decoding RF
+`define BASE_M (`ADDR_W*3+`BASE+1)    // bit base for decoding M
+`define END_M (`BASE_M + `ADDR_W*3)
 
 module Scalar(clk, inst, data_in, acc);
 input clk;
@@ -22,13 +28,13 @@ wire co;
 
 //assign M_out = mult_temp_out[30:15];
 
-Register_File_2R1W u_RF( .clk(clk), .addr_r1(inst[16:13]), .addr_r2(inst[12:9]), .wen_w(inst[21]), .addr_w(inst[20:17]), .data_w(data_write), .rf_data_r1(RF_read_1), .rf_data_r2(RF_read_2), .DM_addr_w1(inst[33:30]), .DM_addr_w2(inst[29:26]), .DM_data(data_in), .DM_wen_1(inst[35]), .DM_wen_2(inst[34]), .DM_addr_r(inst[25:22]), .acc(acc) );       
+Register_File_2R1W u_RF( .clk(clk), .addr_r1(inst[`ADDR_W*2+`BASE-1:`ADDR_W+`BASE]), .addr_r2(inst[`ADDR_W+`BASE-1:`BASE]), .wen_w(inst[`ADDR_W*3+`BASE]), .addr_w(inst[`ADDR_W*3+`BASE-1:`ADDR_W*2+`BASE]), .data_w(data_write), .rf_data_r1(RF_read_1), .rf_data_r2(RF_read_2), .DM_addr_w1(inst[`ADDR_W*3+`BASE_M-1:`BASE_M+`ADDR_W*2]), .DM_addr_w2(inst[`BASE_M+`ADDR_W*2-1:`ADDR_W+`BASE_M]), .DM_data(data_in), .DM_wen_1(inst[`END_M+1]), .DM_wen_2(inst[`END_M]), .DM_addr_r(inst[`ADDR_W+`BASE_M-1:`BASE_M]), .acc(acc) );       
 
 demux_2to3 u_demun2to3( .in(RF_read_1), .sel(inst[4:3]), .y0(M_in_1), .y1(A_in_1), .y2(S_in) );
 demux_1to2 u_demux1to2( .in(RF_read_2), .sel(inst[2]), .y0(M_in_2), .y1(A_in_2) );
 
-DW02_mult #(16, 16) u_mult( .A(M_in_1), .B(M_in_2), .TC(inst[37]), .PRODUCT(M_out) );
-DW01_addsub #(16) u_addsub( .A(A_in_1), .B(A_in_2), .CI(1'b0), .ADD_SUB(inst[36]), .SUM(A_out), .CO(co) );
+DW02_mult #(16, 16) u_mult( .A(M_in_1), .B(M_in_2), .TC(inst[`END_M+3]), .PRODUCT(M_out) );
+DW01_addsub #(16) u_addsub( .A(A_in_1), .B(A_in_2), .CI(1'b0), .ADD_SUB(inst[`END_M+2]), .SUM(A_out), .CO(co) );
 //DW01_ash #(16, 4) u_ash( .A(S_in), .DATA_TC(1'b1), .SH(inst[8:5]), .SH_TC(1'b1), .B(S_out) );
 sh u_sh( .in(S_in), .sel(inst[8:5]), .out(S_out) );
 

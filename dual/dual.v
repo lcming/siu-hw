c21e3318f554215462011beb92eefb2b5a9bbce9
cm@twins <cm99585@hotmail.com>
2015-12-19 23:39:32 +0800

    lint maybe ok

--- dual/dual.v
+++ dual/dual.v
@@ -11,55 +11,69 @@
 `define OFFSET (MEM_W >> 1)
 `define DATA_W 16
 `define LOG_DATA_W 4
-`define INST_W 8
+`define INST_W 6
 
 // stack operation
 
 
-module dual(clk, inst1, inst2, in1, in2, M_sel, A_sel, S_sel, overflow);
+module dual(clk, inst1, inst2, data1, data2, data1_in_en, data2_in_en, data1_out_en, data2_out_en, M_sel, A_sel, S_sel, A_overflow);
 
 parameter PUSH = 2'b00;
 parameter WRITE= 2'b01;
 parameter POP  = 2'b10;
 parameter NOP  = 2'b11;
 
-input clk;
+input clk, data1_in_en, data2_in_en, data1_out_en, data2_out_en;
 input [1:0] M_sel, A_sel, S_sel; // 0: valid 1: select
 input [`INST_W-1:0] inst1, inst2;
-input [`DATA_W-1:0] in1, in2;
-output overflow;
+inout [`DATA_W-1:0] data1, data2;
+output A_overflow;
 
 reg [`DATA_W-1:0] A_buf, M_buf, S_buf;
 reg [`DATA_W-1:0] n_A_buf, n_M_buf, n_S_buf;
 
-wire [`DATA_W-1:0] A_out, M_out, S_out, A_in1, A_in2, M_in1, M_in2, S_in, q1_in, q1_out, s1_data, q2_in, q2_out, s2_data;
+reg [`DATA_W-1:0] A_in1, A_in2, M_in1, M_in2, S_in, q2_in, q1_in;
+
+wire [`DATA_W*2-1:0] _M_out;
+wire [`DATA_W-1:0] A_out, M_out, S_out, q1_out, s1_data, q2_out, s2_data, q1_in_final, q2_in_final;
 wire [1:0] A_in1_sel, M_in1_sel, S_in_sel, s1_op, s2_op;
 wire [2:0] A_in2_sel, M_in2_sel;
-wire q1_push, q1_pop, q2_push, q2_pop, A_cand, M_cand, S_cand;
+wire q1_push, q1_pop, q2_push, q2_pop, A_cand, M_cand, S_cand, A_overflow;
+wire [`LOG_DATA_W-1:0] S_m;
+
+// ping pong control
 
 
 // FU instantiation
-DW02_mult #(`DATA_W, `DATA_W) u_mult ( .A(M_in1), .B(M_in2), .TC(tie_h), PRODUCT(M_out));
-DW01_addsub #(`DATA_W, `DATA_W) u_addsub ( .A(A_in1), .B(A_in2), .CI(tie_l), .ADD_SUB(add_sub), .SUM(A_out));
-DW_rash #(`DATA_W, `LOG_DATA_W) u_rash ( .A(S_in), .DATA_TC(tie_h), .SH(S_m), .SH_TC(tie_h), .B(S_out));
+assign M_out = _M_out[`DATA_W*2-1:`DATA_W];
+DW02_mult #(`DATA_W, `DATA_W) u_mult ( .A(M_in1), .B(M_in2), .TC(1'b1), .PRODUCT(_M_out));
+DW01_addsub #(`DATA_W) u_addsub ( .A(A_in1), .B(A_in2), .CI(1'b0), .ADD_SUB(1'b0), .SUM(A_out), .CO(A_overflow));
+DW_rash #(`DATA_W, `LOG_DATA_W) u_rash ( .A(S_in), .DATA_TC(1'b1), .SH(S_m), .SH_TC(1'b1), .B(S_out));
 
 // RF instantiation
 //module queue(clk, data_in, data_out, push, pop);
-queue q1 (.clk(clk), .data_in(q1_in), .data_out(q1_out), .push(q1_push), .pop(q1_pop));
-queue q2 (.clk(clk), .data_in(q2_in), .data_out(q2_out), .push(q2_push), .pop(q2_pop));
+queue q1 (.clk(clk), .data_in(q1_in_final), .data_out(q1_out), .push(q1_push), .pop(q1_pop));
+queue q2 (.clk(clk), .data_in(q2_in_final), .data_out(q2_out), .push(q2_push), .pop(q2_pop));
 
 //module stack(clk, op, data);
 stack s1 (.clk(clk), .op(s1_op), .data(s1_data));
 stack s2 (.clk(clk), .op(s2_op), .data(s2_data));
 
-// wire connection //
 // stack op
-assign s1_op = inst1[3:4];
-assign s2_op = inst2[3:4];
+assign s1_op = inst1[4:3];
+assign s2_op = inst2[4:3];
 
-// wb 
-assign q1_push = inst1[5];
-assign q2_push = inst2[5];
+// push Q
+assign q1_in_final = inst1[5]? q1_in : data1;
+assign q2_in_final = inst2[5]? q2_in : data2;
+assign q1_push = inst1[5] | data1_in_en;
+assign q2_push = inst2[5] | data2_in_en;
+
+// pop Q
+assign data1 = data1_in_en? `DATA_W'bz : q1_out;
+assign data2 = data2_in_en? `DATA_W'bz : q2_out;
+assign q1_pop = inst1[0] | data1_out_en;
+assign q2_pop = inst2[0] | data2_out_en;
 
 // FU result buffer
 always@(*)
@@ -71,16 +85,17 @@ end
 
 always@(posedge clk)
 begin
-    A_buf <= A_out;
-    M_buf <= M_out;
-    S_buf <= S_out;
+    A_buf <= n_A_buf;
+    M_buf <= n_M_buf;
+    S_buf <= n_S_buf;
 end
+
 
 // MUXs //
 // input of FUs
 always@(*)
-begin: A_IN1
-    case({A_sel[1], inst1[0], inst2[0]})
+begin//: A_IN1
+    casex({A_sel[1], inst1[0], inst2[0]})
         3'b00x: begin A_in1 = q1_out;  end
         3'b01x: begin A_in1 = s1_data; end
         3'b1x0: begin A_in1 = q2_out;  end
@@ -90,8 +105,8 @@ begin: A_IN1
 end
 
 always@(*)
-begin: M_IN1
-    case({M_sel[1], inst1[0], inst2[0]})
+begin//: M_IN1
+    casex({M_sel[1], inst1[0], inst2[0]})
         3'b00x: begin M_in1 = q1_out;  end
         3'b01x: begin M_in1 = s1_data; end
         3'b1x0: begin M_in1 = q2_out;  end
@@ -100,20 +115,28 @@ begin: M_IN1
     endcase
 end
 
+
+assign S_m = S_sel[1]? q2_out[`LOG_DATA_W-1:0] : q1_out[`LOG_DATA_W-1:0];
+
+// special case for shifter in, treat it as IN2
 always@(*)
-begin: S_IN
-    case({S_sel[1], inst1[0], inst2[0]})
-        3'b00x: begin S_in = q1_out;  end
-        3'b01x: begin S_in = s1_data; end
-        3'b1x0: begin S_in = q2_out;  end
-        3'b1x1: begin S_in = s2_data; end
-        default: begin S_in = q1_out; end
+begin//: S_IN
+    casex( {S_sel[1], inst1[2:1], inst2[2:1]} )
+        5'b001xx: begin S_in = A_buf;  end
+        5'b010xx: begin S_in = M_buf;  end
+        5'b011xx: begin S_in = S_buf;  end
+        5'b1xx01: begin S_in = A_buf;  end
+        5'b1xx10: begin S_in = M_buf;  end
+        5'b1xx11: begin S_in = S_buf;  end
+        default : begin S_in = A_buf;  end
     endcase
 end
 
+
+
 always@(*)
-begin: A_IN2
-    case( {A_sel[1], inst1[1:2], inst2[1:2]} )
+begin//: A_IN2
+    casex( {A_sel[1], inst1[2:1], inst2[2:1]} )
         5'b000xx: begin A_in2 = q2_out; end
         5'b001xx: begin A_in2 = A_buf;  end
         5'b010xx: begin A_in2 = M_buf;  end
@@ -127,8 +150,8 @@ begin: A_IN2
 end
 
 always@(*)
-begin: A_IN2
-    case( {M_sel[1], inst1[1:2], inst2[1:2]} )
+begin//: M_IN2
+    casex( {M_sel[1], inst1[2:1], inst2[2:1]} )
         5'b000xx: begin M_in2 = q2_out; end
         5'b001xx: begin M_in2 = A_buf;  end
         5'b010xx: begin M_in2 = M_buf;  end
@@ -143,21 +166,21 @@ end
 
 // output of FUs
 always@(*)
-begin: Q1_IN
-    case( {A_sel[0]&~A_sel[1], M_sel[0]&~M_sel[1], S_sel[0]&~S_sel[1]} )
+begin//: Q1_IN
+    casex( {A_sel[0]&~A_sel[1], M_sel[0]&~M_sel[1], S_sel[0]&~S_sel[1]} )
         3'b1xx: begin q1_in = A_out; end
-        3'bx1x: begin q1_in = M_out; end
-        3'bxx1: begin q1_in = S_out; end
+        3'b01x: begin q1_in = M_out; end
+        3'b001: begin q1_in = S_out; end
         default: begin q1_in = A_out; end
     endcase
 end
 
 always@(*)
-begin: Q2_IN
-    case( {A_sel[0]&A_sel[1], M_sel[0]&M_sel[1], S_sel[0]&S_sel[1]} )
+begin//: Q2_IN
+    casex( {A_sel[0]&A_sel[1], M_sel[0]&M_sel[1], S_sel[0]&S_sel[1]} )
         3'b1xx: begin q2_in = A_out; end
-        3'bx1x: begin q2_in = M_out; end
-        3'bxx1: begin q2_in = S_out; end
+        3'b01x: begin q2_in = M_out; end
+        3'b001: begin q2_in = S_out; end
         default: begin q2_in = A_out; end
     endcase
 end
